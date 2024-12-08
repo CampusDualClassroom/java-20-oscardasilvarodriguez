8a9f1320c05b720764ea484d68ff888dc1205be4
oscardasilvarodriguez <grelodj@hotmail.com>
2024-12-08 21:15:14 +0100

    Ejercicio-20-java resuelto

--- src/main/java/com/campusdual/classroom/Exercise20.java
+++ src/main/java/com/campusdual/classroom/Exercise20.java
@@ -6,11 +6,19 @@ import java.util.List;
 public class Exercise20 {
 
     public static List<Person> getPeopleList() {
-
+        List<Person> stringList = new ArrayList<>();
+        stringList.add(new Person("John", "Smith"));
+        stringList.add(new Teacher("Maria", "Montessori", "Educación"));
+        stringList.add(new PoliceOfficer("Jake", "Peralta", "B-99"));
+        stringList.add(new Doctor("Gregory", "House", "Nefrología e infectología"));
+        return stringList;
     }
 
     public static void showPeopleDetails(List<Person> stringList) {
 
+        for (Person p : stringList) {
+            p.getDetails();
+        }
     }
 
     public static void main(String[] args) {
--- src/main/java/com/campusdual/classroom/Teacher.java
+++ src/main/java/com/campusdual/classroom/Teacher.java
@@ -8,4 +8,9 @@ public class Teacher extends Person {
 		super(name, surname);
 		this.area = area;
 	}
+
+	@Override
+	public void getDetails() {
+		System.out.println("Doctor " + name + " " + surname + ", especialista en " + area.toLowerCase());
+	}
 }
