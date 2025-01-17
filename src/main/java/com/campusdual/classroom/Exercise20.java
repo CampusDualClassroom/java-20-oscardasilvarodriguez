4ccb6fc6bb4a55e18a910e69b92e14f7b431e672
oscardasilvarodriguez <grelodj@hotmail.com>
2025-01-17 09:14:22 +0100

    Ejercicio-20-java refactorizado

--- src/main/java/com/campusdual/classroom/Exercise20.java
+++ src/main/java/com/campusdual/classroom/Exercise20.java
@@ -22,10 +22,6 @@ public class Exercise20 {
     }
 
     public static void main(String[] args) {
-        List<Person> stringList = getPeopleList();
-        showPeopleDetails(stringList);
+        showPeopleDetails(getPeopleList());
     }
-
-
-
 }
